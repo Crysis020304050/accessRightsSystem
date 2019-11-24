c77ea1206747b11f03f060a99dd2850151da28ec
Vasy Terkin <vasy.terkin12@gmail.com>
2019-11-24 15:10:24 +0200

    Create: review

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,55 @@
 'use strict';
 
-const ROLE = Object.freeze( {
+const ROLE = Object.freeze({
     ADMIN: "ADMIN",
     MODERATOR: "MODERATOR",
     USER: "USER",
 
 });
 
-const ACTION = Object.freeze( {
+const ACTION = Object.freeze({
     CREATE: "CREATE",
-    READ:"READ",
+    READ: "READ",
     UPDATE: "UPDATE",
     DELETE: "DELETE",
 });
 
 const permissionMap = new Map();
-permissionMap.set("CREATE","ADMIN, USER");
-permissionMap.set("READ","ADMIN, MODERATOR, USER");
-permissionMap.set("UPDATE","MODERATOR");
-permissionMap.set("DELETE","ADMIN");
 
-function chechPermission(action, role) {
+//значения мы уже прописали. Зачем их снова и снова, и снова, и снова прописывать
+
+/*permissionMap.set("CREATE", "ADMIN, USER");
+permissionMap.set("READ", "ADMIN, MODERATOR, USER");
+permissionMap.set("UPDATE", "MODERATOR");
+permissionMap.set("DELETE", "ADMIN");*/
+
+permissionMap.set(
+    ACTION.CREATE, [
+        ROLE.ADMIN,
+        ROLE.USER,
+    ]);
+permissionMap.set(
+    ACTION.READ, [
+        ROLE.ADMIN,
+        ROLE.MODERATOR,
+        ROLE.USER,
+    ]);
+permissionMap.set(
+    ACTION.UPDATE, [
+        ROLE.MODERATOR,
+    ]);
+permissionMap.set(
+    ACTION.DELETE,
+    [
+        ROLE.ADMIN,
+    ]);
+
+
+//chech?
+//function chechPermission(action, role) {
+function checkPermission(action, role) {
     return permissionMap.get(action).includes(role);
 }
+
+
+//checkPermission(ACTION.DELETE,ROLE.USER);
